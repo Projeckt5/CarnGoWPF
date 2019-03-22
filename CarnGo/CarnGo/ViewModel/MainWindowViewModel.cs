816d5c3feeb3eb76c9fdae8ce88db8ee0a550d1e
Marcus Gasberg <mrgasberg@hotmail.com>
2019-03-22 08:40:17 +0100

    Added a spinner to the default button. Use can be seen in MainWindow and MainWindowViewModel

---
+++ CarnGo/CarnGo/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
+
+namespace CarnGo
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+
+        private ICommand _loadingCommand;
+        private bool _loadingFlag;
+
+        public bool LoadingFlag
+        {
+            get => _loadingFlag;
+            set
+            {
+                if (_loadingFlag == value)
+                    return;
+                _loadingFlag = value;
+                OnPropertyChanged(nameof(LoadingFlag));
+            }
+        }
+
+        public ICommand LoadingCommand
+        {
+            get => _loadingCommand ?? new DelegateCommand(async () =>
+            {
+                if (LoadingFlag)
+                    return;
+
+                LoadingFlag = true;
+                await Task.Delay(2000);
+                LoadingFlag = false;
+            });
+            private set => _loadingCommand = value;
+        }
+    }
+}
