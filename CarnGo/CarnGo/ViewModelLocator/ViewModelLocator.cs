54a40dbb72cd13bea766b4886c8b77d01b7b471d
SuperWeebTanks <trilleo12@live.dk>
2019-03-23 14:42:42 +0100

    Added page navigation classes

---
+++ CarnGo/CarnGo/ViewModelLocator/ViewModelLocator.cs
@@ -0,0 +1,21 @@
+﻿namespace CarnGo
+{
+    public class ViewModelLocator
+    {
+        #region Constructors
+        private ViewModelLocator() { }
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// Singleton instance of ViewModelLocator
+        /// </summary>
+        public static ViewModelLocator Instance { get; } = new ViewModelLocator();
+
+        /// <summary>
+        /// Singleton instance of ViewModelLocator
+        /// </summary>
+        public static ApplicationViewModel ApplicationViewModel { get; set; } = new ApplicationViewModel();
+        #endregion
+    }
+}
