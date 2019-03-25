34e7d75d94e2e77031bc2d31aa8a47a10b7868d6
Hamza <Hamzaabdallah82@gmail.com>
2019-03-25 14:31:37 +0100

    Fixed the view

--- CarnGo/CarnGo/View/LoginView.xaml.cs
+++ CarnGo/CarnGo/View/LoginView.xaml.cs
@@ -2,99 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace CarnGo
+namespace CarnGo.View
 {
-    class LoginPageViewModel : UserModel
+    public partial class LoginView : Page
     {
-        // Made a password here for testing my code.
-        public string Password { get; set; }
-
-        public bool ValidateLoginCredentials { get; set; }
-
-        // initialise
-        public LoginPageViewModel(string email, string password)
+        public LoginView()
         {
-            this.Email = email;
-            // this.Password = password;
+            InitializeComponent();
         }
-        // Validate string - bruges ikke endnu
-        private bool StringValidator(string input)
-        {
-            string pattern = "[^a-zA-Z]";
-
-            if (Regex.IsMatch(input, pattern))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-
-        }
-        //Validate Integer - bruges ikke endnu
-        private bool IntegerValidator(string input)
-        {
-            string pattern = "[^0-9]";
-            if (Regex.IsMatch(input, pattern))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        // Clears the user input so there is an empty field everytime.
-        private void ClearTexts(string email, string password)
-        {
-            email = string.Empty;
-            password = string.Empty;
-        }
-
-
-
-        internal bool LoginCommand(string email, string password)
-        {
-            if (string.IsNullOrEmpty(Email))
-            {
-                MessageBox.Show("Enter the email");
-                return false;
-            }
-            else
-            {
-                if (Email != email)
-                {
-                    MessageBox.Show("Incorrect email");
-                    ClearTexts(email, password);
-                    return false;
-                }
-                else
-                {
-                    if (string.IsNullOrEmpty(password))
-                    {
-                        MessageBox.Show("Enter password");
-                        return false;
-                    }
-                    else if (Password != password)
-                    {
-                        MessageBox.Show("Incorrect password");
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
-
-                }
-            }
-        }
-
-
     }
 }
