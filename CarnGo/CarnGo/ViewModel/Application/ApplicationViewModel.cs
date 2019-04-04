bb834e0d0e3a1e2aa5a2c2fc5f7a56647aff96f1
Edward Brunton <edward.brunton@me.com>
2019-04-04 17:04:48 +0200

    Update ApplicationViewModel.cs

--- CarnGo/CarnGo/ViewModel/Application/ApplicationViewModel.cs
+++ CarnGo/CarnGo/ViewModel/Application/ApplicationViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class ApplicationViewModel : BaseViewModel
     {
-        private ApplicationPage _applicationPage = ApplicationPage.EditUserPage;
+        private ApplicationPage _applicationPage = ApplicationPage.LoginPage;
         private UserModel _currentUser = null;
 
         /// <summary>
