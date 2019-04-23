164fbcf9bcc1d4508c7af0843b9a915f180baf47
Erik Mowinckel <Mowinckel107@gmail.com>
2019-04-23 21:46:19 +0200

    First big Database integration. Making delete function and DB manager functions next

---
+++ CarnGo/CarnGo.Database/Commands.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+using FirstTryProject.Data;
+using FirstTryProject.Model;
+using FirstTryProject.Migrations;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace FirstTryProject
+{
+    class Commands
+    {
+        public static void PullAllData()
+        {
+
+        }
+
+        public static void EmptyDatabase()
+        {
+            using (var db = new AppDbContext())
+            {
+                //foreach (var VARIABLE in db.Cars)
+                {
+                    //db.Cars.Remove(VARIABLE);
+                }
+
+                //foreach (var VARIABLE in db.PossibleToRentDays)
+                {
+                    //db.PossibleToRentDays.Remove(VARIABLE);
+                }
+
+                //foreach (var VARIABLE in db.CarRenters)
+                {
+                    //db.CarRenters.Remove(VARIABLE);
+                }
+
+                //foreach (var VARIABLE in db.CarRenterMessages)
+                {
+                    //db.CarRenterMessages.Remove(VARIABLE);
+                }
+
+                //foreach (var VARIABLE in db.CarOwners)
+                {
+                    //db.CarOwners.Remove(VARIABLE);
+                }
+
+                //foreach (var VARIABLE in db.CarOwnerMessages)
+                {
+                    //db.CarOwnerMessages.Remove(VARIABLE);
+                }
+
+                //foreach (var VARIABLE in db.DaysThatIsRented)
+                {
+                    //db.DaysThatIsRented.Remove(VARIABLE);
+                }
+
+
+                db.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ CarnGo/CarnGo.Database/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace FirstTryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to the database console interface");
+            Console.WriteLine("Your choice of commands are:" +
+                              "\n\"1\" : Pull all data" +
+                              "\n\"2\" : Empty database");
+            do
+            {
+                try
+                {
+                    Console.Write("> ");
+                    var command = Console.ReadLine();
+                    switch (command)
+                    {
+                        case "1":
+                                Commands.PullAllData();
+                            break;
+
+                        case "2":
+                            Commands.EmptyDatabase();
+                            break;
+
+                        default:
+                            Console.WriteLine("Unknown command");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid database request" +
+                                      "\nWant to review exeption?" +
+                                      "\ny/N");
+                    string answer = Console.ReadLine();
+                    if(answer == "y" || answer == "Y")
+                        Console.WriteLine(e);
+                    else if (answer == "n" || answer == "N" || answer == ""){}
+                    else
+                    {
+                        Console.WriteLine("Invalid command, try again");
+                    }
+
+                }
+                
+            } while (true);
+        }
+    }
+}
