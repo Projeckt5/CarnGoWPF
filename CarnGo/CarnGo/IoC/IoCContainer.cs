56f600884baebf49922fd79303e50021a913e463
Martin <martinx1995x@hotmail.com>
2019-04-23 17:54:37 +0200

    hiding header bar

--- CarnGo/CarnGo/IoC/IoCContainer.cs
+++ CarnGo/CarnGo/IoC/IoCContainer.cs
@@ -24,6 +24,7 @@ namespace CarnGo
             Container.RegisterType<IValidator<string>, EmailValidator>(new InjectionConstructor());
             Container.RegisterType<IValidator<SecureString>, PasswordValidator>(new InjectionConstructor());
             Container.RegisterType<IValidator<List<SecureString>>, PasswordMatchValidator>(new InjectionConstructor());
+            Container.RegisterSingleton<MainWindowViewModel>();
             Container.AddExtension(new Diagnostic());
         }
 
