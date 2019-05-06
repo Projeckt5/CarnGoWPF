6da23ebb2ffffb26414ae71db524dc8724bc2bd2
Edward Brunton <edward.brunton@me.com>
2019-05-06 22:58:58 +0200

    Added upload photo functionality to car and user

--- CarnGo/CarnGo/ViewModel/EditUser/EditUserViewModel.cs
+++ CarnGo/CarnGo/ViewModel/EditUser/EditUserViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using CarnGo.Database.Models;
+using Microsoft.Win32;
 using Prism.Commands;
 
 namespace CarnGo
@@ -10,6 +14,7 @@ namespace CarnGo
         private UserModel _userModel;
 
         private ICommand _saveCommand;
+
 
         #endregion
 
@@ -31,10 +36,12 @@ namespace CarnGo
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
+        public BitmapImage UserImage { get; set; }
         #endregion
 
         #region Public Commands
         public ICommand SaveCommand => _saveCommand ?? (_saveCommand = new DelegateCommand(SaveFunction));
+        public ICommand UploadPhotoCommand => new DelegateCommand(() => UploadPhotoFunction());
         #endregion
 
         #region Command Helpers
@@ -44,7 +51,19 @@ namespace CarnGo
             _userModel.Lastname = LastName;
             _userModel.Email = Email;
             _userModel.Address = Address;
+            _userModel.UserImage = UserImage;
             OnPropertyChanged(nameof(UserModel));
+        }
+
+        private void UploadPhotoFunction()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if (fileDialog.ShowDialog() == true)
+            {
+                UserImage = new BitmapImage(new Uri(fileDialog.FileName));
+            }
+            OnPropertyChanged(nameof(UserImage));
         }
 
         #endregion
--- CarnGo/Database/Commands.cs
+++ CarnGo/Database/Commands.cs
@@ -31,48 +31,67 @@ namespace CarnGo.Database
                 var daysRented = db.GetAllDayThatIsRented().Result;
                 var possibleToRent = db.GetAllPossibleToRentDay().Result;
 
-                foreach (var message in messages)
+
+                if (messages != null)
                 {
-                    Console.WriteLine("        Lessor: " + message.LessorEmail);
-                    Console.WriteLine("        Renter: " + message.RenterEmail);
-                    Console.WriteLine("  Confirmation: " + message.Confirmation);
-                    Console.WriteLine("  Message type: " + message.MsgType);
-                    Console.WriteLine("   The message: " + message.TheMessage);
-                    Console.WriteLine("      Has seen: " + message.HaveBeenSeen);
-                    Console.WriteLine("    Message ID: " + message.MessageID);
+                    foreach (var message in messages)
+                    {
+                        Console.WriteLine("        Lessor: " + message.LessorEmail);
+                        Console.WriteLine("        Renter: " + message.RenterEmail);
+                        Console.WriteLine("  Confirmation: " + message.Confirmation);
+                        Console.WriteLine("  Message type: " + message.MsgType);
+                        Console.WriteLine("   The message: " + message.TheMessage);
+                        Console.WriteLine("      Has seen: " + message.HaveBeenSeen);
+                        Console.WriteLine("    Message ID: " + message.MessageID);
+                    }
                 }
 
 
-                foreach (var user in users)
+                if (users != null)
                 {
-                    Console.WriteLine("          Name: " + user.FirstName + " " + user.LastName);
-                    Console.WriteLine("       Address: " + user.Address);
-                    Console.WriteLine("         Email: " + user.Email);
-                    Console.WriteLine("      Password: " + user.Password);
-                    Console.WriteLine("     User type: " + user.UserType);
-                    Console.WriteLine("Number of cars: " + user.Cars.Count);
+                    foreach (var user in users)
+                    {
+                        Console.WriteLine("          Name: " + user.FirstName + " " + user.LastName);
+                        Console.WriteLine("       Address: " + user.Address);
+                        Console.WriteLine("         Email: " + user.Email);
+                        Console.WriteLine("      Password: " + user.Password);
+                        Console.WriteLine("     User type: " + user.UserType);
+                        Console.WriteLine("Number of cars: " + user.Cars.Count);
+                    }
                 }
 
-                foreach (var car in carProfiles)
+
+                if (carProfiles != null)
                 {
-                    Console.WriteLine("         Brand: " + car.Brand);
-                    Console.WriteLine("         Model: " + car.Model);
-                    Console.WriteLine("           Age: " + car.Age);
-                    Console.WriteLine("   Description: " + car.CarDescription);
-                    Console.WriteLine("         Owner: " + car.Owner.FirstName + " " + car.Owner.LastName);
+                    foreach (var car in carProfiles)
+                    {
+                        Console.WriteLine("         Brand: " + car.Brand);
+                        Console.WriteLine("         Model: " + car.Model);
+                        Console.WriteLine("           Age: " + car.Age);
+                        Console.WriteLine("   Description: " + car.CarDescription);
+                        Console.WriteLine("         Owner: " + car.Owner.FirstName + " " + car.Owner.LastName);
+                    }
                 }
 
-                foreach (var possible in possibleToRent)
+
+                if (possibleToRent != null)
                 {
-                    Console.WriteLine("           Car: " + possible.CarProfile.Brand);
-                    Console.WriteLine("          Date: " + possible.Date);
+                    foreach (var possible in possibleToRent)
+                    {
+                        Console.WriteLine("           Car: " + possible.CarProfile.Brand);
+                        Console.WriteLine("          Date: " + possible.Date);
+                    }
                 }
 
-                foreach (var rented in daysRented)
+
+                if (daysRented != null)
                 {
-                    Console.WriteLine("           Car: " + rented.CarProfile.Model);
-                    Console.WriteLine("        Renter: " + rented.User.FirstName + " " + rented.User.LastName);
-                    Console.WriteLine("          Date: " + rented.Date);
+                    foreach (var rented in daysRented)
+                    {
+                        Console.WriteLine("           Car: " + rented.CarProfile.Model);
+                        Console.WriteLine("        Renter: " + rented.User.FirstName + " " + rented.User.LastName);
+                        Console.WriteLine("          Date: " + rented.Date);
+                    }
                 }
 
                 
