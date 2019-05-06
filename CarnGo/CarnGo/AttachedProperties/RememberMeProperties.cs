ecff907ee106cbad721a0b869062ed3c54aace22
Fragarach95 <37145259+Fragarach95@users.noreply.github.com>
2019-05-06 14:28:40 +0200

    Flyttet Remember me FUnktionaliteten til dens egen Klasse under AttachedProperties

---
+++ CarnGo/CarnGo/AttachedProperties/RememberMeProperties.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarnGo
+{
+    public class RememberMeProperties
+    {
+
+
+
+        LoginView Remem = new LoginView();
+
+        //Saving Functions 
+        private void RememberCheck(object sender, RoutedEventArgs e)
+        {
+            {
+                // Save Username
+                Properties.Settings.Default.Username = Remem.Email.Text;
+                Properties.Settings.Default.Save();
+
+                //Save Password
+                Properties.Settings.Default.Password = Remem.Pass.Password;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void EmailLoad(object sender, RoutedEventArgs e)
+        {
+            if (Properties.Settings.Default.Username != string.Empty)
+            {
+                Remem.Email.Text = Properties.Settings.Default.Username;
+            }
+        }
+
+        private void PassLoad(object sender, RoutedEventArgs e)
+        {
+            if (Properties.Settings.Default.Username != string.Empty)
+            {
+                Remem.Pass.Password = Properties.Settings.Default.Password;
+            }
+        }
+
+
+
+    }
+}
