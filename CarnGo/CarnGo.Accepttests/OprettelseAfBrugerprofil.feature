be85da3377f3a5fec1f9bbe4193cb267f8652e0b
MH <mathhan01@gmail.com>
2019-05-20 14:14:19 +0200

    Setup of acceptance test project using Specflow and added first Gherkins scenario

---
+++ CarnGo/CarnGo.Accepttests/OprettelseAfBrugerprofil.feature
@@ -0,0 +1,14 @@
+﻿Egenskab: Oprettelse af brugerprofil
+	Som Lejer / Udlejer
+	Når jeg ikke har en profil endnu
+	Ønsker jeg at kunne registrere mig som bruger
+	Fordi jeg vil kunne gemme og ændre min brugerinfo	
+
+@mytag
+Scenarie: Registrer bruger
+	Givet bruger ønsker at oprette en profil 
+	Når bruger indtaster sin email og password
+	  Og bruger trykker på knappen Register
+	Så bliver oplysningerne gemt i databasen
+	  Og ledes bruger til Login page
+
---
+++ CarnGo/CarnGo.Accepttests/OprettelseAfBrugerprofilSteps.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using CarnGo.Security;
+using NSubstitute;
+using TechTalk.SpecFlow;
+
+namespace CarnGo.Accepttests
+{
+    [Binding]
+    public class OprettelseAfBrugerprofilSteps
+    {
+        private UserSignUpViewModel _uut;
+        private IValidator<string> _fakeEmailValidator;
+        private IValidator<SecureString> _fakePasswordValidator;
+        private IValidator<List<SecureString>> _fakePasswordMatchValidator;
+        private IQueryDatabase _fakeQueryDatabase;
+        private IApplication _fakeApplication;
+
+        [Given(@"bruger ønsker at oprette en profil")]
+        public void GivetBrugerOnskerAtOpretteEnProfil()
+        {
+            _fakeEmailValidator = Substitute.For<IValidator<string>>();
+            _fakePasswordValidator = Substitute.For<IValidator<SecureString>>();
+            _fakePasswordMatchValidator = Substitute.For<IValidator<List<SecureString>>>();
+            _fakeQueryDatabase = Substitute.For<IQueryDatabase>();
+            _fakeApplication = Substitute.For<IApplication>();
+            _uut = new UserSignUpViewModel(_fakeEmailValidator, _fakePasswordValidator, 
+                _fakePasswordMatchValidator, _fakeQueryDatabase, _fakeApplication);
+        }
+        
+        [When(@"bruger indtaster sin email og password")]
+        public void NarBrugerIndtasterSinEmailOgPassword()
+        {
+            _fakeEmailValidator.Validate(Arg.Any<string>()).Returns(true);
+            _fakePasswordValidator.Validate(Arg.Any<SecureString>()).Returns(true);
+            _fakePasswordMatchValidator.Validate(Arg.Any<List<SecureString>>()).Returns(true);
+        }
+        
+        [When(@"bruger trykker på knappen Register")]
+        public void NarBrugerTrykkerPaKnappenRegister()
+        {
+            _uut.RegisterCommand.Execute(null);
+        }
+        
+        [Then(@"bliver oplysningerne gemt i databasen")]
+        public void SaBliverOplysningerneGemtIDatabasen()
+        {
+            _fakeQueryDatabase.Received().RegisterUserTask(
+                Arg.Is<string>(email => email == _uut.Email),
+                Arg.Is<SecureString>(pwd => pwd == _uut.PasswordSecureString));
+        }
+        
+        [Then(@"ledes bruger til Login page")]
+        public void SaLedesBrugerTilLoginPage()
+        {
+            _fakeApplication.Received().GoToPage(ApplicationPage.LoginPage);
+        }
+    }
+}
