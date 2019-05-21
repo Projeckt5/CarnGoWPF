4419e0149d1a8ddbdf3f8e5b8b254a035fd2c1ae
Marcus Gasberg <mrgasberg@hotmail.com>
2019-05-21 11:31:49 +0200

    Added integrationtests for realdbqueries and modelconverters

---
+++ CarnGo/Carngo.Integrationtest/ReaDbQueryDbToAppConverterIntegration.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using CarnGo.Database;
+using CarnGo.Database.Models;
+using CarnGo.Security;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace CarnGo.Integrationtest
+{
+    [TestFixture]
+    public class ReaDbQueryDbToAppConverterIntegration
+    {
+
+        private RealDatabaseQuerier _dbQuerier;
+        private IAppDbContext _fakeDbContext;
+        private ApptoDbModelConverter _appToDbModelConverter;
+        private DbToAppModelConverter _converterToIntegrate;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _fakeDbContext = Substitute.For<IAppDbContext>();
+            _converterToIntegrate = new DbToAppModelConverter();
+            _appToDbModelConverter = new ApptoDbModelConverter();
+            _dbQuerier = new RealDatabaseQuerier(_fakeDbContext,
+                _converterToIntegrate,
+                _appToDbModelConverter);
+        }
+
+        [TestCase("Email", "Password")]
+        [TestCase("email@email", "1234")]
+        [TestCase("1@2.com", "123ikasd")]
+        public async Task GetUser_UserFromDbConverted_UserReturnedCorrectly(string email, string password)
+        {
+            var pwd = password.ConvertToSecureString();
+            _fakeDbContext.GetUser(Arg.Any<string>(), Arg.Any<string>())
+                .Returns(new User()
+                {
+                    Email = email,
+                    Password = password
+                });
+
+            await _dbQuerier.RegisterUserTask(email, pwd);
+            var userResult = await _dbQuerier.GetUserTask(email, pwd);
+
+            Assert.That(userResult.Email, Is.EqualTo(email));
+        }
+    }
+}
--- CarnGo/Carngo.Integrationtest/RealDbQueryAndDbContextIntegration.cs
+++ CarnGo/Carngo.Integrationtest/RealDbQueryAndDbContextIntegration.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using CarnGo.Database;
 using CarnGo.Security;
 using Microsoft.EntityFrameworkCore;
@@ -23,7 +26,15 @@ namespace CarnGo.Integrationtest
             _dbContextToIntegrate = new AppDbContext(options);
             _appToDbModelConverter = new ApptoDbModelConverter();
             _dbToAppModelConverter = new DbToAppModelConverter();
-            _dbQuerier = new RealDatabaseQuerier(_dbContextToIntegrate,_dbToAppModelConverter,_appToDbModelConverter);
+            _dbQuerier = new RealDatabaseQuerier(_dbContextToIntegrate,
+                _dbToAppModelConverter,
+                _appToDbModelConverter);
+        }
+
+        [TearDown]
+        public void TestTearDown()
+        {
+            _dbContextToIntegrate.Database.EnsureDeleted();
         }
 
         [TestCase("Hello@Hello","123456")]
@@ -36,6 +47,175 @@ namespace CarnGo.Integrationtest
             await _dbQuerier.RegisterUserTask(email, pwd);
 
             Assert.That(await _dbContextToIntegrate.Users.AnyAsync());
+        }
+
+        [TestCase("Hello@Hello", "123456")]
+        [TestCase("a@a", "asd123")]
+        [TestCase("asd@123", "asd123")]
+        public async Task GetUser_GetUserFromDb_EmailCorrect(string email, string password)
+        {
+            var pwd = password.ConvertToSecureString();
+            await _dbQuerier.RegisterUserTask(email, pwd);
+
+            var userResult = await _dbQuerier.GetUserTask(email, pwd);
+
+            Assert.That(userResult.Email, Is.EqualTo(email));
+        }
+
+        [TestCase("Hello@Hello", "123456")]
+        [TestCase("a@a", "asd123")]
+        [TestCase("asd@123", "asd123")]
+        public async Task GetUser_GetUserFromDbWithUserModel_EmailCorrect(string email, string password)
+        {
+            var pwd = password.ConvertToSecureString();
+            await _dbQuerier.RegisterUserTask(email, pwd);
+            var user = await _dbQuerier.GetUserTask(email, pwd);
+
+            var userResult = await _dbQuerier.GetUserTask(user);
+
+            Assert.That(userResult.Email, Is.EqualTo(email));
+        }
+
+        [TestCase("Hello@Hello", "123456")]
+        [TestCase("a@a", "asd123")]
+        [TestCase("asd@123", "asd123")]
+        public async Task GetUser_SetAuthorizationString_ThrowsAuthException(string email, string password)
+        {
+            var pwd = password.ConvertToSecureString();
+            await _dbQuerier.RegisterUserTask(email, pwd);
+            var user = await _dbQuerier.GetUserTask(email, pwd);
+            user.AuthorizationString = Guid.NewGuid();
+
+            Assert.ThrowsAsync<AuthorizationFailedException>(async ()=>await _dbQuerier.GetUserTask(user));
+        }
+
+        [TestCase("Hello", "World")]
+        [TestCase("Mads", "Madsen")]
+        [TestCase("Karen", "Jensen")]
+        public async Task UpdateUser_SetFirstAndLastName_FirstAndLastCorrectOnGet(string first, string last)
+        {
+            var pwd = "123asd".ConvertToSecureString();
+            var email = $"{first}@{last}";
+            await _dbQuerier.RegisterUserTask(email, pwd);
+            var user = await _dbQuerier.GetUserTask(email, pwd);
+            user.Firstname = first;
+            user.Lastname = last;
+
+            await _dbQuerier.UpdateUser(user);
+            var userResult = await _dbQuerier.GetUserTask(user);
+
+            Assert.That(userResult.Firstname, Is.EqualTo(first));
+            Assert.That(userResult.Lastname, Is.EqualTo(last));
+        }
+
+        [Test]
+        public async Task UpdateUser_SetAddress_AddressCorrectOnGet()
+        {
+            var pwd = "123asd".ConvertToSecureString();
+            var email = "test@test";
+            await _dbQuerier.RegisterUserTask(email, pwd);
+            var user = await _dbQuerier.GetUserTask(email, pwd);
+            user.Address = "Test Address";
+
+            await _dbQuerier.UpdateUser(user);
+            var userResult = await _dbQuerier.GetUserTask(user);
+
+            Assert.That(userResult.Address, Is.EqualTo(user.Address));
+        }
+
+
+        [Test]
+        public async Task AddUserMessages_UserMessagesInDb_DbContainsAnyMessage()
+        {
+            var pwd = "123asd".ConvertToSecureString();
+            var renterEmail = "renter@test";
+            var lessorEmail = "lessor@test";
+            await _dbQuerier.RegisterUserTask(renterEmail, pwd);
+            await _dbQuerier.RegisterUserTask(lessorEmail, pwd);
+            var renterUser = await _dbQuerier.GetUserTask(renterEmail, pwd);
+            var lessorUser = await _dbQuerier.GetUserTask(lessorEmail, pwd);
+            var carProfile = TestModelFactory.CreateCarProfile();
+            var testMessage = new MessageFromRenterModel(renterUser, lessorUser, carProfile, "TestMessage")
+            {
+                Sender = renterUser,
+                Receiver = lessorUser
+            };
+
+            await _dbQuerier.AddUserMessage(testMessage);
+
+            Assert.That(_dbContextToIntegrate.Messages.Any());
+        }
+
+        [Test]
+        public async Task AddUserMessages_UserMessageJunctionInDb_ContainsAnyMessageAndUser()
+        {
+            var pwd = "123asd".ConvertToSecureString();
+            var renterEmail = "renter@test";
+            var lessorEmail = "lessor@test";
+            await _dbQuerier.RegisterUserTask(renterEmail, pwd);
+            await _dbQuerier.RegisterUserTask(lessorEmail, pwd);
+            var renterUser = await _dbQuerier.GetUserTask(renterEmail, pwd);
+            var lessorUser = await _dbQuerier.GetUserTask(lessorEmail, pwd);
+            var carProfile = TestModelFactory.CreateCarProfile();
+            var testMessage = new MessageFromRenterModel(renterUser, lessorUser, carProfile, "TestMessage")
+            {
+                Sender = renterUser,
+                Receiver = lessorUser
+            };
+
+            await _dbQuerier.AddUserMessage(testMessage);
+
+            Assert.That(_dbContextToIntegrate.MessagesWithUsersJunction.Any());
+        }
+
+        [Test]
+        public async Task GetUserMessages_GetMessagesForRenter_ContainsCorrectMessage()
+        {
+            var pwd = "123asd".ConvertToSecureString();
+            var renterEmail = "renter@test";
+            var lessorEmail = "lessor@test";
+            await _dbQuerier.RegisterUserTask(renterEmail, pwd);
+            await _dbQuerier.RegisterUserTask(lessorEmail, pwd);
+            var renterUser = await _dbQuerier.GetUserTask(renterEmail, pwd);
+            var lessorUser = await _dbQuerier.GetUserTask(lessorEmail, pwd);
+            var carProfile = TestModelFactory.CreateCarProfile();
+            var testMessage = new MessageFromRenterModel(renterUser, lessorUser, carProfile, "TestMessage")
+            {
+                Sender = renterUser,
+                Receiver = lessorUser
+            };
+
+            await _dbQuerier.AddUserMessage(testMessage);
+            var userMessagesResult = await _dbQuerier.GetUserMessagesTask(renterUser,0,10);
+
+            Assert.That(userMessagesResult.Any(model => model.Message == "TestMessage"));
+        }
+
+
+        [Test]
+        public async Task UpdateUserMessages_GetMessagesForRenter_ContainsCorrectMessage()
+        {
+            var pwd = "123asd".ConvertToSecureString();
+            var renterEmail = "renter@test";
+            var lessorEmail = "lessor@test";
+            await _dbQuerier.RegisterUserTask(renterEmail, pwd);
+            await _dbQuerier.RegisterUserTask(lessorEmail, pwd);
+            var renterUser = await _dbQuerier.GetUserTask(renterEmail, pwd);
+            var lessorUser = await _dbQuerier.GetUserTask(lessorEmail, pwd);
+            var carProfile = TestModelFactory.CreateCarProfile();
+            var testMessage = new MessageFromRenterModel(renterUser, lessorUser, carProfile, "TestMessage")
+            {
+                Sender = renterUser,
+                Receiver = lessorUser
+            };
+
+            await _dbQuerier.AddUserMessage(testMessage);
+            var userMessages = await _dbQuerier.GetUserMessagesTask(renterUser, 0, 10);
+            userMessages[0].Message = "UpdatedMessage";
+            await _dbQuerier.UpdateUserMessagesTask(userMessages);
+            var userMessagesResult = await _dbQuerier.GetUserMessagesTask(renterUser, 0, 10);
+
+            Assert.That(userMessagesResult.Any(model => model.Message == "UpdatedMessage"));
         }
     }
 }
